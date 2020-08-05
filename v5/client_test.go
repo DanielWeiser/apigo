cf42e5b417ac9696283f65610401425e748b84b4
Daniel Weiser <sukhorad@retailcrm.ru>
2020-08-05 16:35:06 +0300

    init

--- v5/client_test.go
+++ v5/client_test.go
@@ -9,7 +9,6 @@ import (
 	"net/http"
 	"net/url"
 	"os"
-	"reflect"
 	"strconv"
 	"strings"
 	"testing"
@@ -6628,53 +6627,5 @@ func TestClient_CustomFieldsCreate_Fail(t *testing.T) {
 
 	if customFieldEdit.Success != false {
 		t.Error(successFail)
-	}
-}
-
-func TestClient_OrderDeliveryData(t *testing.T) {
-	d := OrderDeliveryData{
-		OrderDeliveryDataBasic: OrderDeliveryDataBasic{
-			"track",
-			"status",
-			"address",
-			"type",
-		},
-	}
-
-	data, _ := json.Marshal(d)
-	expectedStr := `{"payerType":"type","pickuppointAddress":"address","status":"status","trackNumber":"track"}`
-	if string(data) != expectedStr {
-		t.Error("error")
-	}
-
-	d.AdditionalFields = map[string]interface{}{
-		"customFirst":  "one",
-		"customSecond": "two",
-	}
-
-	data, _ = json.Marshal(d)
-	expectedStr = `{"customFirst":"one","customSecond":"two","payerType":"type","pickuppointAddress":"address","status":"status","trackNumber":"track"}`
-	if string(data) != expectedStr {
-		t.Error("error")
-	}
-
-	d = OrderDeliveryData{}
-	json.Unmarshal(data, &d)
-	expected := OrderDeliveryData{
-		OrderDeliveryDataBasic: OrderDeliveryDataBasic{
-			"track",
-			"status",
-			"address",
-			"type",
-		},
-		AdditionalFields: map[string]interface{}{
-			"customFirst":  "one",
-			"customSecond": "two",
-		},
-	}
-
-	eq := reflect.DeepEqual(expected, d)
-	if eq != true {
-		t.Errorf("Unmarshaled: %#v\nExpected: %#v\n", d, expected)
 	}
 }
